acfc2a6fb8c7af8ee848e4324c9b9014e37e0f80
Sai Komal Potta <komal@Sais-MacBook-Air.local>
2025-02-01 00:25:36 +0530

    Signup & Signin pages are created

--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -1,3 +1,21 @@
+import { Bottomwarning } from "../components/Bottomwarning"
+import { Button } from "../components/Button"
+import { InputBox } from "../components/Inputbox"
+
 export const Signin = () => {
-    return <div> Signin</div>
+    return <div className="bg-slate-300 h-screen flex justify-center">
+        <div className="flex flex-col justify-center">
+            <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
+                <div className="font-bold text-4xl pt-6"> SignIn </div>
+                <div className="text-slate-500 text-md pt-1 px-4 pb-4"> Enter your credentials to access your account </div>
+                <InputBox label={"Email"} placeholder={"komal@gmail.com"} />
+                <InputBox label={"Password"} placeholder={"12345"} />
+                <div className="pt-4">
+                    <Button label={"Sign In"} />
+                </div>
+                <Bottomwarning label={`Don't have an account?`} buttonText={"Sign Up"} to={"/signup" }/>
+            </div>
+        </div>
+    </div>
+
 }
--- frontend/src/pages/signUp.jsx
+++ frontend/src/pages/signUp.jsx
@@ -5,16 +5,18 @@ import { InputBox } from "../components/Inputbox"
 export const Signup = () => {
     return <div className="bg-slate-300 h-screen flex justify-center">
         <div className="flex flex-col justify-center">
-        <div className="font-bold text-4xl pt-6"> SignUp </div>
-        <div className="text-slate-500 text-md pt-1 px-4 pb-4"> Enter your information to create an account </div>
-        <InputBox label={"First Name"} placeholder={"Komal"} />
-        <InputBox label={"Last Name"} placeholder={"Potta"} />
-        <InputBox label={"User Name"} placeholder={"komal@gmail.com"} />
-        <InputBox label={"Password"} placeholder={"12345"} />
-        <div className="pt-4">
-            <Button label={"Sign Up"} />
-        </div>
-        <Bottomwarning label={"Already signed up?"} buttonText={"Sign in"} to={"/signIn"} />
+            <div className="rounded-lg bg-white w-90 text-center p-2 h-max px-4">
+                <div className="font-bold text-4xl pt-6"> SignUp </div>
+                <div className="text-slate-500 text-md pt-1 px-4 pb-4"> Enter your information to create an account </div>
+                <InputBox label={"First Name"} placeholder={"Komal"} />
+                <InputBox label={"Last Name"} placeholder={"Potta"} />
+                <InputBox label={"Email"} placeholder={"komal@gmail.com"} />
+                <InputBox label={"Password"} placeholder={"12345"} />
+                <div className="pt-4">
+                    <Button label={"Sign Up"} />
+                </div>
+                <Bottomwarning label={"Already signed up?"} buttonText={"Sign in"} to={"/signIn"} />
+            </div>
         </div>
     </div>
 
